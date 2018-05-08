a680136cbae707c7df29b2dac8c9d4c3052ef416
ChaoLee <18709270892@163.com>
2018-05-08 18:11:13 +0800

    init

---
+++ src/components/Editor.vue
@@ -0,0 +1,13 @@
+/** 【A4尺寸210mm×297mm】 */
+<template>
+  <div class="bdr-print">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
---
+++ src/components/Layout.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-container class="bdr-layout">
+    <el-header>
+      <h1>报表生成 <small>【A4尺寸210mm×297mm】</small></h1>
+    </el-header>
+    <el-container>
+      <el-aside class="bdr-layout__aside" width="200px">
+        <div class="bdr-taglist">
+          <el-tag
+            v-for="(tag,idx) in tagList"
+            :key="idx"
+            size="mini">{{tag.text}}</el-tag>
+        </div>
+      </el-aside>
+      <el-main>
+        <bdr-editor></bdr-editor>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import BdrEditor from './Editor'
+export default {
+  name: 'HelloWorld',
+  components: {
+    BdrEditor
+  },
+  data () {
+    return {
+      tagList: [{
+        text: 'row'
+      }, {
+        text: 'label'
+      }]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
